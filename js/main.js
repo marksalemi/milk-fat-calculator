05155f6dd5f7b60d989203d29fb295a73f05a9d9
Mark Salemi <salemi.mark@gmail.com>
2020-12-13 14:28:34 -0500

    updated UI

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
-$(document).ready(function(){
+$(document).ready(function() {
+
+
+
+    $(".milk-input").on("input", function() {
+        // Print entered value in a div box
+        alert("Handler for .change() called.");
+    });
 });
